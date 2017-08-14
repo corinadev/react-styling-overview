84c1b5b8ced0991d6401d33c5bd83c82b6d77fd6
Corina Udrescu <corinutz@gmail.com>
2017-08-14 15:41:05 +0200

    Improved table to correctly sort on stars and downloads

--- src/components/LibrariesGrid.js
+++ src/components/LibrariesGrid.js
@@ -7,33 +7,33 @@ const numberFormat = (value) => (parseInt(value, 10).toLocaleString());
 
 const columns = [
   { Header: 'Name',
-    id: 'name',
-    accessor: (library) => (
+    accessor: 'name',
+    Cell: (library) => (
       <a href={library.getGithubUrl()} target='_blank'>
         {library.name}
       </a>)
   },
   { Header: 'Latest version', accessor: 'version' },
   { Header: 'Created at',
-    id: 'createdAt',
-    accessor: (library) => (dateFormat(library.createdAt))},
+    accessor: 'createdAt',
+    Cell: (library) => (dateFormat(library.createdAt))},
   { Header: 'Description', accessor: 'description' },
   { Header: 'Author', accessor: 'author' },
   { Header: 'NPM Downloads',
-    id: 'downloads',
-    accessor: (library) => (numberFormat(library.downloads))
+    accessor: 'downloads',
+    Cell: (library) => (numberFormat(library.downloads))
   },
   { Header: 'Github stargazers',
-    id: 'stars',
-    accessor: (library) => (numberFormat(library.stars))
+    accessor: 'stars',
+    Cell: (library) => (numberFormat(library.stars))
   },
   {
     Header: 'Topics',
-    id: 'topics',
+    accessor: 'topics',
     minWidth: 300,
-    accessor: (library) => (library.topics ? library.topics.join(', ') : ''),
+    Cell: (library) => (library.topics ? library.topics.join(', ') : ''),
     filterMethod: (filter, library) => {
-      return library.topics.indexOf(filter.value) !== -1;
+      return library.topics && library.topics.indexOf(filter.value) !== -1;
     }
   }
 ];
